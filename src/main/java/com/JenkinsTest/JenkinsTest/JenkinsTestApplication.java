90fd766e43ad08ac73a8ddcb7476fdb26daeab43
hrithik+2-at-970287672632 <hrithik@salesgear.io>
2025-10-05 21:18:59 +0530

    first commit

---
+++ src/main/java/com/JenkinsTest/JenkinsTest/JenkinsTestApplication.java
@@ -0,0 +1,14 @@
+package com.JenkinsTest.JenkinsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("JenkinsTestApplication");
+		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+
+}
